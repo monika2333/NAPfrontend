890c12ec164db73c0fbcb1a7b37bd4519fe61814
liuwt <liuwt@liuwtdeMacBook-Pro.local>
2024-01-14 15:54:31 +0800

    fixes:编译静态文件

--- dist/static/js/vue3-seamless-scroll.es-da7d6895.js
+++ dist/static/js/vue3-seamless-scroll.es-da7d6895.js
@@ -1,4 +1,4 @@
-import{ao as zI,b as GI,r as hr,x as jr,J as Yh,aj as FI,z as HI,j as un,n as WI,F as $I,A as UI}from"./index-4a887e02.js";/*! *****************************************************************************
+import{ao as zI,b as GI,r as hr,x as jr,J as Yh,aj as FI,z as HI,j as un,n as WI,F as $I,A as UI}from"./index-b243cd54.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
